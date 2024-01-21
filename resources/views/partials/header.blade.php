ec6419cda54422aebc4b5ed133533bf619ff1e93
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-22 00:19:20 +0100

    css debug

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,30 @@
 <header>
     <nav class="container">
         <div class="row">
-            <div id="logo" class="col-4 d-flex align-items-center justify-content-center">
+            <div id="logo" class="col-3 d-flex align-items-center justify-content-center">
                 <img src="../assets/img/dc-logo.png" alt="">
             </div>
-            <div id="links" class="col-8 d-flex align-items-center justify-content-end">
-                <a class="mx-3" href=""></a>
+            <div id="links" class="col-9 d-flex align-items-center">
+                <ul class="footer-item d-flex align-items-center justify-content-around">
+                    <li class="text-secondary"><a class="footer-link" href="#">Characters</a></li>
+                    <li class="text-secondary"><a class="footer-link" href="#">Comics</a></li>
+                    <li class="text-secondary"><a class="footer-link" href="#">Movies</a></li>
+                    <li class="text-secondary"><a class="footer-link" href="#">TV</a></li>
+                    <li class="text-secondary"><a class="footer-link" href="#">Games</a></li>
+                    <li class="text-secondary"><a class="footer-link" href="#">Videos</a></li>
+                    <li class="text-secondary"><a class="footer-link" href="#">News</a></li>
+                    <ul class="footer-item pe-0">
+                    </ul>
             </div>
         </div>
     </nav>
 </header>
 
 <style scoped>
+    .text-secondary {
+        margin: 0 30px;
+    }
+
     header {
         height: 100px;
     }
